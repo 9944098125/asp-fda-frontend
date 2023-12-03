4b65082434936e7ff2229c641119eabd7c5e8876
Srinivas Akella <srinivas72075@gmail.com>
2023-12-03 14:50:49 +0530

    updated api endpoint

--- src/Redux/Api/Api.js
+++ src/Redux/Api/Api.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const Api = Axios.create({
 	// baseURL: "http://localhost:5000/api",
-	baseURL: "https://order-food-0hlv.onrender.com/api",
+	baseURL: "https://asp-foa-node.onrender.com/api",
 });
 
 export default Api;
