9d5278b40e416330fc56b02b93905582d312dc77
Srinivas Akella <srinivas72075@gmail.com>
2023-12-08 16:13:59 +0530

    added

--- src/Redux/Actions/login.js
+++ src/Redux/Actions/login.js
@@ -3,36 +3,37 @@ import Api from "../Api/Api";
 import { alertActions } from "./alert";
 
 export const login = (body) => async (dispatch) => {
-  dispatch({
-    type: LOGIN_START,
-  });
-  try {
-    const res = await Api.post("/auth/login", body, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (res) {
-      dispatch({
-        type: LOGIN_SUCCESS,
-        payload: res.data,
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    dispatch({
-      type: LOGIN_FAIL,
-      payload: err.response?.data.message,
-    });
-    dispatch(alertActions.error(err.response?.data.message));
-    setTimeout(() => {
-      dispatch(alertActions.clear());
-    }, 3000);
-  }
+	dispatch({
+		type: LOGIN_START,
+	});
+	try {
+		const res = await Api.post("/auth/login", body, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		if (res) {
+			dispatch({
+				type: LOGIN_SUCCESS,
+				payload: res.data,
+			});
+		}
+		console.log(res.data);
+	} catch (err) {
+		console.log(err);
+		dispatch({
+			type: LOGIN_FAIL,
+			payload: err.response?.data.message,
+		});
+		dispatch(alertActions.error(err.response?.data.message));
+		setTimeout(() => {
+			dispatch(alertActions.clear());
+		}, 3000);
+	}
 };
 
 export const logout = () => (dispatch) => {
-  dispatch({
-    type: LOGOUT,
-  });
+	dispatch({
+		type: LOGOUT,
+	});
 };
