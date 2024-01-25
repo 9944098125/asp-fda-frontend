6342a34eb0fc38ba7ad204d053e060bdc08ea2e4
Srinivas Akella <srinivas72075@gmail.com>
2024-01-25 11:18:36 +0530

    added ssrs

--- src/Pages/SSRS/index.jsx
+++ src/Pages/SSRS/index.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-export default function Ssrs() {
+const Ssrs = () => {
+	const openExternalLink = (url) => {
+		window.open(url, "_blank");
+	};
+
 	return (
-		<React.Fragment>
-			<div className="p-5">
-				<h1 className="text-success fs-4">
-					CLICK THE BELOW BUTTON TO HAVE A LOOK AT SSRS REPORTS:
-				</h1>
-				<Link
-					to="http://65.0.84.221/Reports/report/Report_for_Embedding_Task/Cross_tabulation"
-					style={{ textDecoration: "none", color: "grey" }}>
-					<button className="btn btn-warning py-2 px-5">Click Me !</button>
-				</Link>
-			</div>
-		</React.Fragment>
+		<div className="p-5">
+			<p className="fs-4 text-success">
+				Click the link to navigate to an external page without a full reload:
+			</p>
+			<button className="btn btn-warning py-2 px-5 text-white">
+				<a
+					href="http://65.0.84.221/Reports/report/Report_for_Embedding_Task/Cross_tabulation"
+					target="_blank"
+					rel="noopener noreferrer"
+					onClick={(e) => {
+						e.preventDefault();
+						openExternalLink(e.currentTarget.href);
+					}}
+					style={{ textDecoration: "none", color: "inherit" }}>
+					External Link
+				</a>
+			</button>
+		</div>
 	);
-}
+};
+
+export default Ssrs;
